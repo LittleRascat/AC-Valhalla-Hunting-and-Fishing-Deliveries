b59e649e63fbefc7d63c9312926b70d2f6bd1219
Roman <royr0212@gmail.com>
2021-05-29 10:50:24 -0500

    Fininshed delivery functionality, started fish totals.

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,35 @@
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
-  let status = incomplete;
-  if (status = incomplete) {
-    button.addEventListener('click', () => {
-      button.style.cssText = 'background: lightgreen';
-    });
-    status = complete;
-  } else {
-    button.addEventListener('click', () => {
-      button.style.cssText = 'background: white';
-    });
-    status = incomplete;
-  }
+  let status = 0;
+  button.addEventListener('click', () => {
+    let statusColors = ['lightgreen', 'cornsilk'];
+    button.style.background = statusColors[status];
+    status++;
+    if (status === statusColors.length) {status = 0;}
+  });
+});
+
+let regularEel = 4;
+
+const container = document.querySelector('#regular-eel');
+
+const content = document.createElement('p');
+content.classList.add('content');
+content.textContent = `${regularEel}`;
+
+container.appendChild(content);
+
+
+let offering2 = document.getElementById("offering-2");
+let offering2Status = 0;
+
+offering2.addEventListener('click', () => {
+  let statusColors = [-2, 2];
+  regularEel += statusColors[offering2Status];
+  offering2Status++;
+  if (offering2Status === statusColors.length) {offering2Status = 0;}
+  content.textContent = `${regularEel}`;
 });
 
 
@@ -23,4 +40,9 @@ buttons.forEach((button) => {
 
 
 
-  
+
+
+
+
+
+
